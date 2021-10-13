f3e7ec451d1f6633acc241f29f055c28dec66b22
phuongtung1 <phuongtung1231992@yahoo.com>
2021-10-13 14:34:12 +0800

    update

--- version.js
+++ version.js
@@ -2,6 +2,49 @@ const { gitDescribeSync } = require('git-describe');
 const { version } = require('./package.json');
 const { resolve, relative } = require('path');
 const { writeFileSync } = require('fs-extra');
+var fs = require('fs');
+var path = require('path');
+
+
+function copyFileSync( source, target ) {
+
+    var targetFile = target;
+
+    // If target is a directory, a new file with the same name will be created
+    if ( fs.existsSync( target ) ) {
+        if ( fs.lstatSync( target ).isDirectory() ) {
+            targetFile = path.join( target, path.basename( source ) );
+        }
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync( source, target ) {
+    var files = [];
+
+    // Check if folder needs to be created or integrated
+    var targetFolder = path.join( target, path.basename( source ) );
+    if ( !fs.existsSync( targetFolder ) ) {
+        fs.mkdirSync( targetFolder );
+    }
+
+    // Copy
+    if ( fs.lstatSync( source ).isDirectory() ) {
+        files = fs.readdirSync( source );
+        files.forEach( function ( file ) {
+            var curSource = path.join( source, file );
+            if ( fs.lstatSync( curSource ).isDirectory() ) {
+                copyFolderRecursiveSync( curSource, targetFolder );
+            } else {
+                copyFileSync( curSource, targetFolder );
+            }
+        } );
+    }
+}
+
+copyFolderRecursiveSync('./node_modules/@design-automation/mobius-sim/typedoc-json', './src/assets')
+
 
 const gitInfo = gitDescribeSync({
     dirtyMark: false,
@@ -19,4 +62,3 @@ export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
 `, { encoding: 'utf-8' });
 
 console.log(`Wrote version info ${gitInfo.raw} to ${relative(resolve(__dirname, '..'), file)}`);
-
