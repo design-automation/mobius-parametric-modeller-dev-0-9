c3f1c0b4e3690772874c0ec5b3a50aef18e729b5
phuongtung1 <phuongtung1231992@yahoo.com>
2022-01-17 17:17:46 +0800

    function return statement detection update

--- src/app/shared/functions/process-functions.ts
+++ src/app/shared/functions/process-functions.ts
@@ -6,71 +6,19 @@ import { IFunction } from '@models/procedure';
 import * as showdown from 'showdown';
 
 // const doc = require('@assets/typedoc-json/doc.json');
-const mdConverter = new showdown.Converter({literalMidWordUnderscores: true, simpleLineBreaks: true});
+const mdConverter = new showdown.Converter({ literalMidWordUnderscores: true, simpleLineBreaks: true });
 const module_list = {};
-const extraMods = [ 'variable', 'comment', 'expression',
-                    'control_flow', 'global_func', 'local_func',
-                    'dashboard', 'editor', 'flowchart', 'gallery', 'menu',
-                    'console', 'geoviewer', 'cadviewer', 'vrviewer'];
+const extraMods = ['variable', 'comment', 'expression',
+    'control_flow', 'global_func', 'local_func',
+    'dashboard', 'editor', 'flowchart', 'gallery', 'menu',
+    'console', 'geoviewer', 'cadviewer', 'vrviewer'];
 const asyncFuncList = ['io.Read', 'io.Write', 'io.Import', 'io.Export', 'io._getFile',
-                       'util.ModelCompare', 'util.ModelMerge'];
+    'util.ModelCompare', 'util.ModelMerge'];
 
-// todo: bug fix for defaults
-function extract_params(func: Function): [IArgument[], boolean] {
-    const fnStr = func.toString().replace( /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg, '');
-    let result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).split(','); // .match( /([^\s,]+)/g);
-    if (result === null || result[0] === '') {
-         result = [];
-    }
-    const final_result = result.map(function(r) {
-        r = r.trim();
-        const r_value = r.split('=');
+/**
+ * DOCUMENTATION
+ */
 
-        if (r_value.length === 1) {
-            return { name: r_value[0].trim(), value: undefined};
-        } else {
-            return { name: r_value[0].trim(), value: undefined};
-        }
-
-    });
-    let hasReturn = true;
-    if (fnStr.indexOf('return') === -1 || fnStr.indexOf('return;') !== -1) {
-        hasReturn = false;
-    }
-    return [final_result, hasReturn];
-}
-const mobiusFuncs = new Funcs()
-const moduleNames = Object.getOwnPropertyNames(mobiusFuncs)
-for ( const m_name of moduleNames ) {
-    // if (!Funcs[m_name] || (typeof Funcs[m_name] !== 'object')) { continue; }
-    if (m_name === '__model__' || m_name.endsWith('Model')) { continue; }
-    const modObj = {'__enum__': mobiusFuncs[m_name].__enum__};
-    const funcNames = Object.getOwnPropertyNames(Object.getPrototypeOf(mobiusFuncs[m_name]))
-    for ( const fn_name of funcNames) {
-        if (fn_name === 'constructor') { continue; }
-
-        const func = mobiusFuncs[m_name][fn_name];
-
-        const fnObj = <IFunction>{};
-        fnObj.module = m_name;
-        fnObj.name = fn_name;
-        if (asyncFuncList.indexOf(`${m_name}.${fn_name}`) !== -1) {
-            const paramFunc = mobiusFuncs[m_name]['_Async_Param_' + fn_name]
-            fnObj.argCount = paramFunc.length;
-            const args = extract_params(paramFunc);
-            fnObj.args = args[0];
-            fnObj.hasReturn = args[1];
-            modObj[fn_name] = fnObj;
-        } else {
-            fnObj.argCount = func.length;
-            const args = extract_params(func);
-            fnObj.args = args[0];
-            fnObj.hasReturn = args[1];
-            modObj[fn_name] = fnObj;
-        }
-    }
-    module_list[m_name] = modObj;
-}
 function analyzeParamType(fn, paramType) {
     if (paramType.type === 'array') {
         return `${analyzeParamType(fn, paramType.elementType)}[]`;
@@ -89,7 +37,6 @@ function analyzeParamType(fn, paramType) {
         return paramType.type;
     }
 }
-
 function addDoc(mod, modName, docs) {
     const moduleDoc = {};
     if (mod.comment && mod.comment.shortText) {
@@ -113,7 +60,8 @@ function addDoc(mod, modName, docs) {
             }
             if (cmmt.tags) {
                 for (const fnTag of cmmt.tags) {
-                    if (fnTag.tag === 'summary') { fn['summary'] = fnTag.text;
+                    if (fnTag.tag === 'summary') {
+                        fn['summary'] = fnTag.text;
                     } else {
                         if (fn[fnTag.tag]) {
                             fn[fnTag.tag].push(fnTag.text);
@@ -161,41 +109,6 @@ function addDoc(mod, modName, docs) {
     docs[modName] = moduleDoc;
 }
 
-function addModFuncDoc(modDoc, modUrl, modName) {
-    fetch(modUrl).then(res => {
-        if (!res.ok) {
-            console.log('HTTP Request Error: Unable to retrieve documentation for ' + modName);
-            return '';
-        }
-        const mod = {};
-        modDoc[modName] = mod;
-        res.text().then(docText => {
-            const splitText = docText.split('## ');
-            if (splitText.length === 1) {
-                const funcText = docText.split('# ')[1];
-                const funcName = funcText.split('\n')[0].trim().toLowerCase();
-
-                if (extraMods.indexOf(modName) !== -1) {
-                    mod[funcName] = '## ' + funcText.trim();
-                } else {
-                    mod[funcName] = '## ' + modName + '.' + funcText.trim();
-                }
-            } else {
-                for (const funcText of splitText) {
-                    if (funcText[0] === '#') { continue; }
-                    const funcName = funcText.split('\n')[0].trim().toLowerCase();
-
-                    if (extraMods.indexOf(modName) !== -1) {
-                        mod[funcName] = '## ' + funcText.trim();
-                    } else {
-                        mod[funcName] = '## ' + modName + '.' + funcText.trim();
-                    }
-                }
-            }
-        });
-    });
-}
-
 const moduleDocs = {};
 const inlineDocs = {};
 // const functionDocs = {};
@@ -210,6 +123,70 @@ for (const mod of doc.children) {
 for (const mod of inlinedoc.children) {
     if (mod.name.toLowerCase().startsWith('inline')) { continue; }
     addDoc(mod, mod.name, inlineDocs);
+}
+
+/**
+ * MAIN FUNCTIONS
+ */
+
+function extract_params(func: Function): [IArgument[], boolean] {
+    const fnStr = func.toString().replace(/((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg, '');
+    let result = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).split(','); // .match( /([^\s,]+)/g);
+    if (result === null || result[0] === '') {
+        result = [];
+    }
+    const final_result = result.map(function (r) {
+        r = r.trim();
+        const r_value = r.split('=');
+
+        if (r_value.length === 1) {
+            return { name: r_value[0].trim(), value: undefined };
+        } else {
+            return { name: r_value[0].trim(), value: undefined };
+        }
+
+    });
+    let hasReturn = true;
+    if (fnStr.indexOf('return') === -1 || fnStr.indexOf('return;') !== -1) {
+        hasReturn = false;
+    }
+    return [final_result, hasReturn];
+}
+const mobiusFuncs = new Funcs();
+const moduleNames = Object.getOwnPropertyNames(mobiusFuncs);
+
+for (const m_name of moduleNames) {
+    if (m_name === '__model__' || m_name.endsWith('Model')) { continue; }
+    const modObj = { '__enum__': mobiusFuncs[m_name].__enum__ };
+    const funcNames = Object.getOwnPropertyNames(Object.getPrototypeOf(mobiusFuncs[m_name]))
+    for (const fn_name of funcNames) {
+        if (fn_name === 'constructor') { continue; }
+
+        const func = mobiusFuncs[m_name][fn_name];
+
+        const fnObj = <IFunction>{};
+        fnObj.module = m_name;
+        fnObj.name = fn_name;
+
+        if (asyncFuncList.indexOf(`${m_name}.${fn_name}`) !== -1) {
+            const paramFunc = mobiusFuncs[m_name]['_Async_Param_' + fn_name]
+            fnObj.argCount = paramFunc.length;
+            const args = extract_params(paramFunc);
+            fnObj.args = args[0];
+            fnObj.hasReturn = args[1];
+            modObj[fn_name] = fnObj;
+        } else {
+            fnObj.argCount = func.length;
+            const args = extract_params(func);
+            fnObj.args = args[0];
+            fnObj.hasReturn = args[1];
+            modObj[fn_name] = fnObj;
+        }
+        if (moduleDocs[m_name] && moduleDocs[m_name][fn_name] && moduleDocs[m_name][fn_name].returns) {
+            fnObj.hasReturn = moduleDocs[m_name][fn_name].returns.indexOf('void') === -1;
+        }
+    }
+    module_list[m_name] = modObj;
 }
 export const ModuleList = module_list;
 export const ModuleDocList = moduleDocs;
