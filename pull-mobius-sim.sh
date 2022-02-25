a6caf13d35b0cdc34241ccc9f44b805228f68ef4
phuongtung1 <phuongtung1231992@yahoo.com>
2022-02-25 10:14:12 +0800

    update

--- pull-mobius-sim.sh
+++ pull-mobius-sim.sh
@@ -8,16 +8,16 @@ fi
 
 if cd mobius-sim;
 then git pull; cd ..;
-else git clone https://github.com/design-automation/mobius-sim.git mobius-sim;
+else git clone https://github.com/design-automation/mobius-sim.git;
 fi
 
 if cd mobius-sim-funcs;
 then git pull; cd ..;
-else git clone https://github.com/design-automation/mobius-sim-funcs.git mobius-sim-funcs;
+else git clone https://github.com/design-automation/mobius-sim-funcs.git;
 fi
 
 if cd mobius-inline-funcs;
 then git pull; cd ..;
-else git clone https://github.com/design-automation/mobius-inline-funcs.git mobius-inline-funcs;
+else git clone https://github.com/design-automation/mobius-inline-funcs.git;
 fi
 
